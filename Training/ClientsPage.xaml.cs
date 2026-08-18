36be8c216142e088b2da9c5771cea27c86915870
GS1-HUB <2241539@my.ipleiria.pt>
2026-08-19 00:06:19 +0100

    .

--- Training/ClientsPage.xaml.cs
+++ Training/ClientsPage.xaml.cs
@@ -1,9 +1,50 @@
+using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+using System.Data;
+
 namespace Training;
 
 public partial class ClientsPage : ContentPage
 {
-	public ClientsPage()
+    private readonly IConfiguration _configuration;
+
+    public ClientsPage(IConfiguration configuration)
 	{
 		InitializeComponent();
+		_configuration = configuration;
 	}
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+        var clientes = GetClientes();
+        ClientsCollectionView.ItemsSource = clientes;
+    }
+
+    private List<Dictionary<string, object?>> GetClientes()
+    {
+        var clientes = new List<Dictionary<string, object?>>();
+        string connectionString = _configuration["ConnectionStrings:Default"]
+            ?? throw new InvalidOperationException("Connection string não encontrada");
+
+        using (var conn = new MySqlConnection(connectionString))
+        {
+            conn.Open();
+            string query = @"SELECT * FROM clientes";
+
+            using var cmd = new MySqlCommand(query, conn);
+            using var reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                clientes.Add(new Dictionary<string, object?>
+                {
+                    ["id"] = reader.GetInt32("id"),
+                    ["nome"] = reader.GetString("nome"),
+                    ["cidade"] = reader.GetString("cidade"),
+                    ["data_registo"] = reader.GetDateTime("data_registo")
+                });
+            }
+        }
+        return clientes;
+    }
 }
